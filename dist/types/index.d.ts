f471a8566b2472c823b98cae209a134d8578cc89
Andrew Kishino <AndrewKishino@gmail.com>
2020-02-28 18:51:07 -0800

    Prepare package update

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -3,6 +3,6 @@ export { default as Key } from './key';
 export { default as crypto } from './crypto';
 export { default as forge } from './forge';
 export { default as utility } from './utility';
-export { default as ledger } from 'ledger';
+export { default as ledger } from './ledger';
 export { default as constants } from './constants';
 export default Sotez;
